7f69a4494a5db97f9bb6094f69f61833c131b5fd
Vincent Kaufmann <vkaufmann@MBP-Vincent.local>
2024-03-02 21:51:00 +0100

    second commit

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def show_history():
 #     app.run('0.0.0.0', debug=True, port=5000)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host="0.0.0.0", port=port)
